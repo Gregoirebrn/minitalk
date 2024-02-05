77c5a2d57f5f73d2685ef7fbd61ced1acd723f00
Grgoirebrn <gregoire2246@gmail.com>
2024-02-05 14:52:32 +0100

    added message receiver

---
+++ minitalk.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: grebrune <grebrune@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/02 02:09:47 by grebrune          #+#    #+#             */
+/*   Updated: 2024/02/05 00:37:19 by grebrune         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include "libftbis/libft.h"
+#include <signal.h>
+
+#endif
---
+++ serveur.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   serveur.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: grebrune <grebrune@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/02 02:14:04 by grebrune          #+#    #+#             */
+/*   Updated: 2024/02/05 01:21:16 by grebrune         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+
+int	main(void)
+{
+	ft_putstr_fd("PID : ", 1);
+	ft_putnbr_fd(getpid(), 1);
+	ft_putstr_fd("\n", 1);
+	send_str(SIGINT, handle_signal);
+	while (1)
+		pause();
+	return (0);
+}
