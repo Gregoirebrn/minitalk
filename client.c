93da4b14562113d048c6effa9e433fa1db408954
Grgoirebrn <gregoire2246@gmail.com>
2024-02-05 14:51:35 +0100

    add src/ in libftbis

---
+++ client.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: grebrune <grebrune@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/02 02:11:04 by grebrune          #+#    #+#             */
+/*   Updated: 2024/02/05 02:16:48 by grebrune         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+static	void	send_str(pid_t pid, char *str)
+{
+	int		i;
+	int		x;
+	char	c;
+
+	x = 0;
+	while (str && str[x])
+	{
+		i = 8;
+		c = str[x];
+		while (i--)
+		{
+			if (c >> i & 1)
+				kill(pid, SIGUSR1);
+			else
+				kill(pid, SIGUSR2);
+			usleep(100);
+		}
+		x++;
+	}
+	i = 8;
+	while (i--)
+	{
+		kill(pid, SIGUSR1);
+		usleep(100);
+	}
+}
+
+int	main(int ac, char **av)
+{
+	pid_t pid;
+
+	if (ac < 3 || !av[2])
+		return (2);
+	ft_putstr_fd("Sended at ", 1);
+	ft_putnbr_fd(ft_strlen(av[2]), 1);
+	ft_putstr_fd("Received : ", 1);
+	pid = atoi(av[1]);
+	send_str(pid, av[2]);
+	while (1)
+		pause();
+	return (0);
+}
