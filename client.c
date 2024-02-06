2c17b99f39dcab6350c6f2e3332702405bfcfb6e
grebrune <grebrune@student.42lyon.fr>
2024-02-06 19:52:31 +0100

    added answer of the srv to cli

--- client.c
+++ client.c
@@ -6,11 +6,26 @@
 /*   By: grebrune <grebrune@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/02 02:11:04 by grebrune          #+#    #+#             */
-/*   Updated: 2024/02/05 17:17:01 by grebrune         ###   ########.fr       */
+/*   Updated: 2024/02/06 19:46:03 by grebrune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+static void	signal_handler(int sig)
+{
+	static int	rec_bit = 0;
+
+	if (sig == SIGUSR1)
+		rec_bit++;
+	if (sig == SIGUSR2)
+	{
+		ft_putstr_fd("Finish Sending ", 1);
+		ft_putnbr_fd(rec_bit, 1);
+		ft_putstr_fd(" bits.\n", 1);
+		exit (0);
+	}
+}
 
 static	void	send_str(pid_t pid, char *str)
 {
@@ -43,7 +58,7 @@ static	void	send_str(pid_t pid, char *str)
 
 int	main(int ac, char **av)
 {
-	pid_t	pid;
+	pid_t			pid;
 
 	if (ac < 3 || !av[2])
 		return (2);
@@ -51,6 +66,8 @@ int	main(int ac, char **av)
 	ft_putnbr_fd(atoi(av[1]), 1);
 	ft_putstr_fd("\n", 1);
 	pid = atoi(av[1]);
+	signal(SIGUSR1, signal_handler);
+	signal(SIGUSR2, signal_handler);
 	send_str(pid, av[2]);
 	while (1)
 		pause();
--- serveur.c
+++ serveur.c
@@ -6,31 +6,36 @@
 /*   By: grebrune <grebrune@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/02 02:14:04 by grebrune          #+#    #+#             */
-/*   Updated: 2024/02/05 17:10:52 by grebrune         ###   ########.fr       */
+/*   Updated: 2024/02/06 19:49:36 by grebrune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	signal_handler(int sig)
+void	signal_handler(int sig, siginfo_t *info, void *ucontext)
 {
 	static pid_t			pid_cli = 0;
 	static unsigned char	received_c = 0;
 	static int				byte_count = 0;
 
+	(void)ucontext;
 	if (pid_cli == 0)
-		pid_cli = getpid();
+		pid_cli = info->si_pid;
 	if (sig == SIGUSR2)
 	{
 		received_c <<= 1;
 		byte_count++;
+		kill(pid_cli, SIGUSR1);
 	}
 	else if (sig == SIGUSR1)
 	{
 		received_c <<= 1;
 		received_c |= 1;
 		byte_count++;
+		kill(pid_cli, SIGUSR1);
 	}
+	if (byte_count == 8 && received_c == '\0')
+		kill(pid_cli, SIGUSR2);
 	if (byte_count == 8)
 	{
 		ft_putchar_fd(received_c, 1);
@@ -46,7 +51,8 @@ int	main(void)
 	ft_putstr_fd("PID : ", 1);
 	ft_putnbr_fd(getpid(), 1);
 	ft_putstr_fd("\n", 1);
-	s_sig.sa_handler = signal_handler;
+	s_sig.sa_sigaction = signal_handler;
+	s_sig.sa_flags = SA_SIGINFO;
 	sigaction(SIGUSR1, &s_sig, 0);
 	sigaction(SIGUSR2, &s_sig, 0);
 	while (1)
