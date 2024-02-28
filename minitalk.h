4c49c219bb59fd48fe67e4e38eb5e5bd88c194b5
grebrune <grebrune@student.42lyon.fr>
2024-02-28 17:51:18 +0100

    del lib unused

--- minitalk.h
+++ minitalk.h
@@ -6,15 +6,13 @@
 /*   By: grebrune <grebrune@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/02 02:09:47 by grebrune          #+#    #+#             */
-/*   Updated: 2024/02/05 00:37:19 by grebrune         ###   ########.fr       */
+/*   Updated: 2024/02/28 17:48:14 by grebrune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 
-# include <sys/types.h>
-# include <sys/wait.h>
 # include <unistd.h>
 # include "libftbis/libft.h"
 # include <signal.h>
